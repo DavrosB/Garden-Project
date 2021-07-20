7dc8a5624011ce76838a2377bd0f887073c7f4e7
DavrosB <85989267+DavrosB@users.noreply.github.com>
2021-07-20 09:28:26 +0100

    Added backend files
    Java files

---
+++ Backend/SpringExercisesGardenApplication.java
@@ -0,0 +1,13 @@
+package com.qa.spring.garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExercisesGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExercisesGardenApplication.class, args);
+	}
+
+}
